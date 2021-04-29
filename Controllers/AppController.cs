d9b683b827e54c1638cfb49ffcccef8fd0264879
KevserAkbas <45363875+KevserAkbas@users.noreply.github.com>
2021-04-29 23:52:10 +0300

    Controller ve View oluşturuldu. Razor Kodu Eklendi #3

---
+++ Controllers/AppController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    //Bu sınıf MVC içindeki Controller Adlı bir sınıftan türetilecek
+    public class AppController : Controller
+    {
+        //Controller, belirli bir eyleme gelen bir isteği eşlememize izin verir ve
+        //bu eylem gerçek mantığın gerçekleşeceği yerdir.
+
+        // IActionResult --> arabirim döndüren yöntemlerden biri
+        // IActionResult --> burada olanları almanın, onu bir view a eşleştirmenin ve
+        // nihayetinde geri getirmenin yollarından biridir. 
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
