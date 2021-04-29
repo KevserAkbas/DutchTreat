273b1b12b38eda76f343e60fa919e0030479efae
KevserAkbas <45363875+KevserAkbas@users.noreply.github.com>
2021-04-29 21:56:25 +0300

    JQuery Alıştırması #2

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,17 +1,32 @@
-﻿var x = 0;
-var s = "";
-/*console.log --> tarayıcıda konsola yazar */
-console.log("Hello Pluralsight");
+﻿////belge bir jQuery nesnesi etrafında sarıldı
+//// ready --> tarayıcı hazır olur olmaz bu kod bloğunda ne varsa çalıştırılacağını söyler.
+//2. avantajı tüm bu şeyler kendi işlevinde gizlenmesi ve böylece içindeki tüm kodun birbirini tanımasıdır.
+$(document).ready(function () {
+    var x = 0;
+    var s = "";
+    /*console.log --> tarayıcıda konsola yazar */
+    console.log("Hello Pluralsight");
 
-var theForm = $("#theForm");
-theForm.hide();
+    var theForm = $("#theForm");
+    theForm.hide();
 
-var button = $("#byButton");
-button.on("click", function () {
-    console.log("Satın alma eşyası");
-});
+    var button = $("#byButton");
+    button.on("click", function () {
+        console.log("Satın alma eşyası");
+    });
 
-var productInfo = $(".product-props li");
-productInfo.on("click", function () {
-    console.log("You clicked on" + $(this).text());
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on" + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.slideToggle(1000);
+    });
+
+
+
 });
