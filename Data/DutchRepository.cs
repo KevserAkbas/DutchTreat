372081fae31a06a8fc9cf5aa53c02cddc180542a
KevserAkbas <45363875+KevserAkbas@users.noreply.github.com>
2021-05-05 02:01:38 +0300

    Hatalar günlüğe nasıl kaydedilir #5 konsolda *set ASPNETCORE_ENVIRONMENT=Development * diyerek böylece uygulamamızı geliştirme aşamasında çalıştırdığımızda gerçekte nasıl göründüğünü görebiliriz. *dotnet run - dersek bazı bilgi etiketlerini göreceğiz çünkü günlük (log) tutma varsayılan olarak konsola yazmak üzere ayarlanmıştır *Dolayısıyla, projede startup ında bile, eklenen bazı günlükleri görürüz, * tek tek sayfalara gitmeye başlarsak ve konsola geri dönersek, bir çok mesaj görürüz. (aynı şeyleri output da ASP:NET Core Web Server da da görebiliriz) * StaticFiles a yapılan her istek günlüğe kaydediliyor *Günlüğe kaydetme düzeyini doğrudan yapılandırma(configuration) dosyasında yapılandırabiliriz * bu yüzden config.json u açalım ve Logging adında yeni bir bölüm ekleyelim ve LogLevel i günlük kaydının içinde yuvalanmış bir nesne olarak ayarlayacağız ve sonra varsayılan diyeceğiz, uyarı diyelim *  "Logging": {     "LogLevel": {       "Default" : "Warning"     }   } * bu, ona varsayılan bir günlük kaydı düzeyi olarak kullanılmasını söyleyecektir, * uyarıdır ve bu nedenle gördüğümüz bu mesajların çoğu kaybolmalıdır. *konsolda durdurud yeniden başlatırsak bilgi mesajlarının çoğunun kaybolduğunu görürüz *kendi kodumuzda günlük tutmanın neye benzediğini görmek için * DutchRepository e gidelim ve ILogger adında ikinci bir üye enjekte edelim * böylece veri yaydığında, günlüğün nereden geldiğini gerçekten görebileceğiz. * GetAllProducts çağırrdığımızda, bilgileri günlüğe kaydetmek için logger kullanalım. *tekrar konsola dönüp projeyi yeniden başlatıyoruz *"Logging": {     "LogLevel": {       "Default": "Information",       "Microsoft": "Warning"     }   } *burada Microsoft kelimesiyle başlayan her kaydedicinin Uyarı kullandığını söylemektir. *hatta default yerine DutchTreat diyebiliriz çünkü kaydedicimiz (DutchRepository), hatırlarsak bu tür DutchTreat.Data.DucthReporitory kelimesiyle başlar, *  bu yüzden bunu oldukça iyi filtrelemek için kullanabilirz işlemleri yaptıktan projeyi tekrar çalıştırıp (dotnet run) *sayfayı yeniler ve konsola geri dönersek mesajımıszı görebiliriz

--- Data/DutchRepository.cs
+++ Data/DutchRepository.cs
@@ -1,4 +1,5 @@
 ﻿using DutchTreat.Data.Entities;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,16 +10,29 @@ namespace DutchTreat.Data
     public class DutchRepository : IDutchRepository
     {
         private readonly DutchContext _ctx;
+        private readonly ILogger<DutchContext> _logger;
 
-        public DutchRepository(DutchContext ctx)
+        public DutchRepository(DutchContext ctx, ILogger<DutchContext> logger)
         {
             _ctx = ctx;
+            _logger = logger;
         }
         public IEnumerable<Product> GetAllProducts() //tüm ürünlerin listesini alacaktır.
         {
-            return _ctx.Products
-                    .OrderBy(p => p.Title)
-                    .ToList();
+            try
+            {
+                _logger.LogInformation("GetAllProducts was celled ");
+
+                return _ctx.Products
+                        .OrderBy(p => p.Title)
+                        .ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to get all products: {ex}");
+                return null;
+            }
+            
         }
         // bu API'nin kullanıcısından bizi istedikleri kategoride geçirmesini isteyeceğiz
         public IEnumerable<Product> GetProductByCategory(string category)
