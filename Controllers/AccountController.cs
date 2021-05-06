99cf81f868db8c939bebc822bcde5eba27cb8762
KevserAkbas <45363875+KevserAkbas@users.noreply.github.com>
2021-05-06 21:11:27 +0300

    Login View Tasarımı #7 * RedirectToAction - belirli bir controllar a ve eyleme yönlendirmedir *return RedirectToAction("Index", "App");  - eylem adı ,Index  - controller adı, App

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    public class AccountController:Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            if (this.User.Identity.IsAuthenticated)//birisi zaten giriş yapmış mı 
+            {//zaten oturum açılmışsa, oturum açma sayfası kullanışlı değildir
+                //bu nedenle insanların yanlışlıkla giriş yapmamalarını ve kafalarının karışmamasını sağlama için
+                return RedirectToAction("Index", "App");
+            }
+            return View();
+        }
+    }
+}
